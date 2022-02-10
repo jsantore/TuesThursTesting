1bfdcb0f9c30c18f093c58e4754c8754404f1af6
jsantore <jsantore@bridgew.edu>
2022-02-10 10:34:26 -0500

    more tests

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ def find_distance(point1, point2):
     delta_y = point1[1]-point2[1]
     result = math.sqrt(delta_x*delta_x+delta_y*delta_y)
     return result
+
+
+def report_results(results: list[dict], file_to_print=None):
+    for item in results:
+        print(item, file=file_to_print)
+    print("this is junk ", file=file_to_print)
+
+
+def main():
+    data = [{"name": "comp490", "cap": 20, "credits": 3},
+            {"name": "comp151", "cap": 24, "credits": 4},
+            {"name": "math161", "cap": 20, "credits": 5}]
+    report_results(data)
+
+if __name__ == '__main__':
+    main()
--- tests/testDemo.py
+++ tests/testDemo.py
@@ -14,7 +14,22 @@ def test_distance():
     result3 = main.find_distance((-3, -5), (-3, -5))
     assert result3 == 0
 
-def test_bad_data():
 
-    result = main.find_distance((2, 4, 6,), (3, 5, 7))
+def test_bad_data():
+    with pytest.raises(ValueError):
+        result = main.find_distance((2, 4, 6,), (3, 5, 7))
+
+def test_report_results():
+    data = [{"name": "comp490", "cap": 20, "credits": 3},
+            {"name": "comp152", "cap": 24, "credits": 4},
+            {"name": "comp390", "cap": 18, "credits": 3}]
+    with open("testOutput.txt", 'w') as outfile:
+        main.report_results(data, outfile)
+    with open("testOutput.txt") as testfile:
+        allLines = testfile.readlines()
+        assert len(data) == len(allLines)
+        count = 0
+        for line in allLines:
+            assert line.strip() == str(data[count])
+            count += 1
 
